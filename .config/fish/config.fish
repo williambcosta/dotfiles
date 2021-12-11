92e718fb6a80395e4cce53e4d360fd68b54c73b5
William Batista Costa <costa.williambatista@gmail.com>
2021-12-10 21:52:36 -0300

    Add the fish config file

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+function fish_greeting
+end
+
+# Bindings
+function fish_user_key_bindings
+    bind \cC kill-whole-line
+    bind \cH backward-kill-bigword
+end
+
+# "Alias" to help versionate the dotfiles it calls git passing $HOME/.dotfiles
+# as the bare directory and $HOME as the working tree plus the arguments typed
+function dfconfig
+    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
+end
