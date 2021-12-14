d03b7dd2ffbd8f4a2a4180f9ab6c260d6f65e47f
William Batista Costa <costa.williambatista@gmail.com>
2021-12-14 20:47:16 -0300

    Update fish config file
    Change back the right arrow behavior to move one char forward

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,6 @@ end
 function fish_user_key_bindings
     bind \cC kill-whole-line
     bind \cH backward-kill-bigword
-    bind \e\[C forward-word
 end
 
 # "Alias" to help versionate the users dotfiles. It calls git passing $HOME/.dotfiles
