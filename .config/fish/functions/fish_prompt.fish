db52e1ede4c5c345c53e04f5ac51142518612634
William Batista Costa <costa.williambatista@gmail.com>
2022-01-11 20:54:19 -0300

    Add custom fish_prompt function

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,36 @@
+# Adapted from Informative VCS
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+    set -l color_cwd
+    set -l suffix
+
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        else
+            set color_cwd $fish_color_cwd
+        end
+        set suffix '#>'
+    else
+        set color_cwd $fish_color_cwd
+        set suffix '>'
+    end
+
+    # PWD
+    set_color $color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
+
+    printf '%s ' (fish_vcs_prompt)
+
+    set -l status_color (set_color $fish_color_status)
+    set -l statusb_color (set_color --bold $fish_color_status)
+    set -l prompt_status (__fish_print_pipestatus "[" "] " "|" "$status_color" "$statusb_color" $last_pipestatus)
+    echo -n "$prompt_status"
+    set_color normal
+
+    set_color $color_cwd
+    echo -n "$suffix "
+end
